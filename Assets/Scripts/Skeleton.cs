2b3bdf3a77e616240746fc99e2b4a77b03211be0
Zachary Luebke <zach.verex@gmail.com>
2018-09-11 09:12:39 -0400

    Skeleton movement.

--- Assets/Scripts/Skeleton.cs
+++ Assets/Scripts/Skeleton.cs
@@ -5,19 +5,54 @@ using UnityEngine.Networking;
 
 public class Skeleton : NetworkBehaviour {
 
+	[SerializeField] private float moveSpeed = 1.0f;
+	[SerializeField] private bool isMoving;
+	private Vector3 startPosition;
+	private Vector3 endPosition;
+	private float t;
+
 	private IEnumerator SlowUpdate() {
 		while (true) {
-			RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.right, 1.0f);
 
-			if (hit.collider != null) {
-				Debug.Log("We hit something.");
-				yield return new WaitForSeconds(200.0f);
+			/*
+				RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.right, 1.0f);
+
+				if (hit.collider != null) {
+					Debug.Log("We hit something.");
+					yield return new WaitForSeconds(200.0f);
+				}
+			 */
+
+			//transform.position += new Vector3(1, 0, 0);
+
+			if (!isMoving) {
+				Debug.Log("Move");
+				StartCoroutine(Move());
+			} else {
+				Debug.Log("No move.");
 			}
 
-			transform.position += new Vector3(1, 0, 0);
-
-			yield return new WaitForSeconds(2.0f);
+			yield return new WaitForSeconds(0.1f);
 		}
+	}
+
+	private IEnumerator Move() {
+		isMoving = true;
+		startPosition = transform.position;
+
+		endPosition = new Vector3(startPosition.x + 1, startPosition.y, startPosition.z);
+
+		float factor = 1.0f;
+
+		while (t < 1.0f) {
+			t += Time.deltaTime * moveSpeed * factor;
+			transform.position = Vector3.Lerp(startPosition, endPosition, t);
+			yield return null;
+		}
+
+		isMoving = false;
+
+		yield return 0;
 	}
 
 	// Use this for initialization
