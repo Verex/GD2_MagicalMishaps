2e98e5dedcf72c3e8a0018341dc6223163748821
Zach <zach.verex@gmail.com>
2018-09-14 18:30:30 -0400

    Working on pathfinding.

--- Assets/Scripts/Grid/GridSystem.cs
+++ Assets/Scripts/Grid/GridSystem.cs
@@ -4,58 +4,165 @@ using UnityEngine;
 using UnityEngine.Tilemaps;
 
 [RequireComponent(typeof(Tilemap))]
-public class GridSystem : MonoBehaviour {
+public class GridSystem : MonoBehaviour
+{
 
-	[SerializeField] private int defaultCost = 100;
-	[SerializeField] private TileCost[] tileCosts;
+    [SerializeField] private int defaultCost = 100;
+    [SerializeField] private TileCost[] tileCosts;
 
-	private Tilemap tilemap;
-	private Grid grid;
+	[SerializeField] private Vector2Int gridSize;
 
-	// Use this for initialization
-	void Start () {
-		// Get main components.
-		tilemap = GetComponent<Tilemap>();
-		grid = GetComponentInParent<Grid>();
+    private List<Step> StepList = new List<Step>(0);
 
-		Vector3Int p = grid.WorldToCell(new Vector3(10, 10, 0));
-		TileBase tb = tilemap.GetTile(p);
+    private List<Vector2Int> MovementOptions = new List<Vector2Int> { new Vector2Int(0, 1) };
 
-		foreach(TileCost tc in tileCosts)
-		{
-			Debug.Log(tc.name);
-		}
-	}
+    private Tilemap tilemap;
+    private Grid grid;
 
-	public static List<Vector3Int> FindPath(Vector3Int start, Vector3Int end)
+    // Use this for initialization
+    void Start()
+    {
+        // Get main components.
+        tilemap = GetComponent<Tilemap>();
+        grid = GetComponentInParent<Grid>();
+
+        Vector3Int p = grid.WorldToCell(new Vector3(10, 10, 0));
+        TileBase tb = tilemap.GetTile(p);
+
+        foreach (TileCost tc in tileCosts)
+        {
+            Debug.Log(tc.name);
+        }
+    }
+
+	private void MessageCurrent(Vector2Int position, IList<Vector2Int> path)
 	{
-		// Clear our step list.
+		StepList.Add(new Step(StepType.Current, position, path));
+	}
 
-		// Check for start/end.
-		if (start == end)
+	private void MessageClose(Vector2Int position)
+	{
+		StepList.Add(new Step(StepType.Close, position, new List<Vector2Int>(0)));
+	}
+
+	private List<Vector2Int> PartiallyReconstructPath (Vector2Int start, Vector2Int end, Vector2Int[] cameFrom)
+	{
+		List<Vector2Int> path = new List<Vector2Int> { end };
+
+		return path;
+	}
+
+    private List<Vector2Int> ReconstructPath(Vector2Int start, Vector2Int end, Vector2Int[] cameFrom)
+    {
+        List<Vector2Int> path = new List<Vector2Int> { end };
+        Vector2Int current = end;
+        do
+        {
+            Vector2Int prev = cameFrom[GetIndexUnchecked(current.x, current.y)];
+            current = prev;
+            path.Add(current);
+        } while (current != start);
+
+        return path;
+    }
+
+    public List<Vector2Int> FindPath(Vector2Int start, Vector2Int end, int iterationLimit = 200)
+    {
+        // Clear step list.
+        StepList.Clear();
+
+        // Check for start/end.
+        if (start == end)
+        {
+            return new List<Vector2Int> { start };
+        }
+
+        MinHeapNode head = new MinHeapNode(start, ManhattanDistance(start, end));
+		MinHeap open = new MinHeap();
+
+		// Push head node to heap.
+		open.Push(head);
+
+		// Define total cost.
+		float[] totalCost = new float[gridSize.x * gridSize.y];
+		Vector2Int[] cameFrom = new Vector2Int[gridSize.x * gridSize.y];
+
+		while(open.HasNext() && iterationLimit > 0)
 		{
-			return new List<Vector3Int> { start };
+			// Get current position.
+			Vector2Int current = open.Pop().Position;
+			MessageCurrent(current, PartiallyReconstructPath(start, end, cameFrom));
+
+			if (current == end)
+			{
+                return ReconstructPath(start, end, cameFrom);
+			}
+
+            Step(open, cameFrom, totalCost, current, end);
+
+            MessageClose(current);
+
+            --iterationLimit;
 		}
 
+        return new List<Vector2Int>();
+    }
 
-		return new List<Vector3Int>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private void Step(MinHeap open, Vector2Int[] cameFrom, float[] totalCost, Vector2Int current, Vector2Int end)
+    {
+        float initialCost = totalCost[GetIndexUnchecked(current.x, current.y)];
+
+        foreach (Vector2Int option in MovementOptions)
+    }
+
+    private List<Vector2Int> GetMovementOptions(Vector2Int position)
+    {
+        List<Vector2Int> moves = new List<Vector2Int>();
+
+        foreach (Vector2Int move in MovementOptions)
+        {
+            Vector2Int target = position + move;
+
+            if (target.x >= 0 && target.y < gridSize.x && target.y >= 0 && target.y < gridSize.y)
+            {
+                moves.Add(move);
+            }
+        }
+
+        return moves;
+    }
+
+    private int GetIndexUnchecked(int x, int y)
+    {
+        return gridSize.x * y + x;
+    }
+
+    private static float ManhattanDistance(Vector2Int p0, Vector2Int p1)
+    {
+        var dx = Mathf.Abs(p0.x - p1.x);
+        var dy = Mathf.Abs(p0.y - p1.y);
+        return dx + dy;
+    }
 }
 
-public class Step {
+public class Step
+{
 
-	public StepType Type;
-	public Vector3Int Position;
-	public IList<Vector3Int> Path;
+    public Step(StepType type, Vector2Int position, IList<Vector2Int> path)
+    {
+        this.Type = type;
+        this.Position = position;
+        this.Path = path;
+    }
+
+    public StepType Type;
+    public Vector2Int Position;
+    public IList<Vector2Int> Path;
 }
 
-public enum StepType {
-	Current,
-	Open,
-	Close
+public enum StepType
+{
+    Current,
+    Open,
+    Close
 }
---
+++ Assets/Scripts/Grid/Pathfinding/MinHeapNode.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class MinHeapNode {
+	public MinHeapNode(Vector2Int position, float expectedCost)
+	{
+		this.Position = position;
+		this.ExpectedCost = expectedCost;            
+	}
+
+	public Vector2Int Position;
+	public float ExpectedCost;       
+	public MinHeapNode Next;
+}
